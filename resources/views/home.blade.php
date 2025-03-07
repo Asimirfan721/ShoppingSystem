27d71cdd33512705eac7c9f1ab02a4d8fc133202
Asim Irfan <rajputasim721@gmail.com>
2025-03-07 10:09:21 +0500

    middleware issue resolved completely

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -160,8 +160,7 @@
             <a href="{{ route('shirts') }}" class="category-btn">Shirts</a>
             <a href="{{ route('watches') }}" class="category-btn">Watches</a>
             <a href="{{ route('create') }}" class="category-btn">Create</a>
-            <a href="{{ route('New-1') }}" class="category-btn">1-New Button</a>
-            <a href="{{ route('Second') }}" class="category-btn">Second</a>
+           
         </div>
 
         <!-- Loading Spinner -->
